2abdfac589d8314eb106ffb10a535073a176c591
yousef <yousefbh@gmail.com>
2025-05-24 18:20:05 +0300

    added time interrupt, just need cli flag now

--- quiz.go
+++ quiz.go
@@ -6,11 +6,10 @@ import (
 	"log"
 	"os"
 	"strconv"
-	"sync"
 	"time"
 )
 
-func quiz() (int, int) {
+func quiz(timeout <-chan time.Time) (int, int) {
 	f, err := os.Open("problems.csv")
 	if err != nil {
 		log.Fatal(err)
@@ -28,20 +27,26 @@ func quiz() (int, int) {
 	numQuestions := 0
 	for _, row := range equations {
 		equation := row[0]
-		numQuestions += 1
 		answer, err := strconv.Atoi(row[1])
 		if err != nil {
 			log.Fatal(err)
 		}
 		fmt.Println(equation)
-		var input int
 		fmt.Print("Enter answer: ")
-		_, err1 := fmt.Scan(&input)
-		if err1 != nil {
-			log.Fatal(err)
-		}
-		if answer == input {
-			numCorrect += 1
+		answerCh := make(chan int, 1)
+		go func() {
+			var in int
+			fmt.Scan(&in)
+			answerCh <- in
+		}()
+		select {
+		case <-timeout:
+			return numCorrect, (numQuestions + 1)
+		case input := <-answerCh:
+			if input == answer {
+				numCorrect++
+				numQuestions++
+			}
 		}
 	}
 	return numCorrect, numQuestions
@@ -49,34 +54,12 @@ func quiz() (int, int) {
 
 func main() {
 
-	done := make(chan struct{})
 	start := time.Now()
+	timeout := time.After(3 * time.Second)
 
-	go func() {
-		ticker := time.NewTicker(1 * time.Second)
-		defer ticker.Stop()
-		seconds := 0
-		for {
-			select {
-			case <-done:
-				return
-			case <-ticker.C:
-				seconds++
-				fmt.Printf("time elapsed %d seconds\n", seconds)
-			}
-		}
-	}()
-
-	var wg sync.WaitGroup
-	wg.Add(1)
 	// numCorrect, numQuestions := go quiz()
 	var numCorrect, numQuestions int
-	go func() {
-		defer wg.Done()
-		numCorrect, numQuestions = quiz()
-		close(done)
-	}()
-	wg.Wait()
+	numCorrect, numQuestions = quiz(timeout)
 	end := time.Since(start)
 	fmt.Printf("You got %d / %d correct\n", numCorrect, numQuestions)
 	fmt.Printf("and took %v seconds\n", end)
